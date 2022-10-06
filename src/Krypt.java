59538467394f114bd46758be922dd7c74958fe82
Indianeen <axel.lundberg@elev.ga.ntig.se>
2022-10-06 13:40:25 +0200

    mycket lite gjort

---
+++ src/Krypt.java
@@ -0,0 +1,34 @@
+
+import javax.swing.*;
+
+public class Krypt {
+    String fileInput = "ld";
+    String fileOutput = "df";
+    String Key = "cd";
+    public static void main (){
+
+    }
+}
+class Model {
+// ta emot och beräkna
+    /**ta emot fil eller text*/
+
+    /**läsa fil eller text*/
+
+    /**fil/text ^ key*/
+
+    /**spara output*/
+
+
+
+}
+class View {
+    /**input string/file*/
+    String fileinput = JOptionPane.showInputDialog();
+    /**input key*/
+    /***/
+
+}
+class Controller {
+    // transportera
+}
